c1cd38c13c99c09e0c1b1f12403f98743f8d4606
MarianoHernandez <mariano.escobal@cencosud.com.uy>
2023-07-31 20:06:57 -0300

    cambios

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import { Provider } from "react-redux";
-import "./App.css";
-//import RegisterForm from './componentes/RegisterForm';
-import LoginForm from "./componentes/LoginForm";
-
+import { Provider } from 'react-redux';
+import './App.css';
+import { store } from './store/store'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RegisterForm from './componentes/RegisterForm';
+import LoginForm from './componentes/LoginForm';
 import { store } from "./store/store";
+
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <LoginForm />
+        <BrowserRouter>
+          <Routes>
+              <Route path="/login" element={<LoginForm />}></Route>
+              <Route path="/registro" element={<RegisterForm />}></Route>
+              <Route path="/logout" element={<Contacto />}></Route>
+              {/* <Route path="/dashboard" element={<Analisis />}>
+                <Route path='/agregar'></Route>  
+                <Route path='/listadopersona'></Route>  //Filtro
+                <Route path='/totale'></Route>  
+              </Route> */}
+              <Route path="*" element={<NoEncontrado />}></Route>
+          </Routes>
+        </BrowserRouter>
+
       </Provider>
     </div>
   );
